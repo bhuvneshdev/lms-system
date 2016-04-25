1161c529ad6c4b941dbf9cf1f3d94cfea364c4ad
bhuvnesh <--replace-all>
2016-04-24 22:50:10 -0700

    changes

--- db/migrate/20160406070138_add_type_book.rb
+++ db/migrate/20160406070138_add_type_book.rb
@@ -1,6 +1,6 @@
 class AddTypeBook < ActiveRecord::Migration
   def change
-  	add_column :books, :type , :string
+  	change_column :books, :type , :string
   	add_column :books, :sub_type , :string
   end
 end
