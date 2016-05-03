423fbd4643720aee21fe84e3813004c7b5907ae8
bhuvnesh <--replace-all>
2016-05-02 18:43:06 -0700

    few changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,18 @@ class Post < ActiveRecord::Base
 	self.inheritance_column = :_type_disabled
 
 
+	def email_comments
+		id = self.id
+		arr = []
+		cat1 = self.sub_category
+		# binding.pry
+		Post.all.each do |x|
+			if (!x.sub_category.nil? && !cat1.nil? && (x.sub_category.downcase.include?(cat1.downcase) || cat1.downcase.include?(x.sub_category.downcase)))
+				x.comments.each {|x| arr << x.email_id if !x.email_id.nil?}
+			end
+		end
+		return arr.uniq
+	end
 
 	def before_save_tasks
 		self.body = self.body.gsub("\n","<br>")
